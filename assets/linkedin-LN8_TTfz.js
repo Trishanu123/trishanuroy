0f474f0fbe59549418e74508ba90e1580b8230cb
Trishanu Roy <gtrishanu@gmail.com>
2025-03-20 17:51:48 +0530

    Updates

--- assets/linkedin-LN8_TTfz.js
+++ assets/linkedin-LN8_TTfz.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-DL1FR5z3.js";/**
+import{c as e}from"./index-C69Nku1e.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
