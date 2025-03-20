e040003773674d79c927afe82a9341c145618ebb
Trishanu Roy <gtrishanu@gmail.com>
2025-03-20 17:50:01 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: "/", // Make sure this matches your repo name in lowercase
   optimizeDeps: {
-    exclude: ['lucide-react'],
-    
+    exclude: ['lucide-react']
   },
   assetsInclude: ['**/*.pdf'],
 });
